575b429dd65a3b457a69ba43c4b74fcd91807270
Benny Rahmat <46495960+akunbeben@users.noreply.github.com>
2026-04-23 10:03:04 +0700

    Updated the nix-darwin switch command to add sudo

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Other Shell Script Commands
+
+# ...
+
+# Original command on line 99
+# nix run nix-darwin -- switch --flake ~/Projects/home#Macbook
+# Updated command
+sudo nix run nix-darwin -- switch --flake ~/Projects/home#Macbook
+
+# Other Shell Script Commands
+
+# ...
